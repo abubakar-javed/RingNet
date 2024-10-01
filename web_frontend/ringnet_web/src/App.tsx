f3f06c42e394f55028e4ab6fd8faee57625d502b
Abubakar Javed <abubakarjaved03145005454@gmail.com>
2024-10-01 14:24:52 +0500

    Login Page and Register page shift done..

--- web_frontend/ringnet_web/src/App.tsx
+++ web_frontend/ringnet_web/src/App.tsx
@@ -1,48 +1,40 @@
-import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import axios from "axios";
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setToken } from './State/authSlice.js';
+import { Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './Components/ProtectedRoute.js';
+import OAuthCallback from './Components/OAuthCallback.js';
+import LoginPage from "./Pages/LoginPage.js"
+import DashboardPage from "./Pages/DashboardPage.js"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log("hereee")
-  const fetchEarthquakes = async () => {
-    try {
-      const { data } = await axios.get(
-        `${import.meta.env.VITE_BACKEND_URL}/api/earthquake`);
-      console.log(data)
-    } catch (err) {
-    } 
-  };
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetchEarthquakes();
-  }, []);
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(setToken(token));
+    }
+  }, [dispatch]);
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/auth/callback" element={<OAuthCallback />}></Route>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/auth/callback" element={<OAuthCallback />}></Route>
+          </Route>
+        </Routes>
       </div>
-      <h1>RingNet</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ToastContainer />
+
     </>
   )
 }
 
-export default App
+export default App;
